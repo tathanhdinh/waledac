cb08b54f852a2ba2680995d65104c8df1ff2dd7e
eric <therond.eric@online.fr>
2010-06-10 13:50:36 +0200

    blba

--- src/vtkTimerCallback.cpp
+++ src/vtkTimerCallback.cpp
@@ -4,7 +4,6 @@
 vtkTimerCallback::vtkTimerCallback(vtkBotnetGraph *ptrbotnetgraph)
 {
 	this->ptrbotnetgraph = ptrbotnetgraph;
-	this->TimerCount = 0;
 	this->first_time = true;
 }
  
@@ -14,25 +13,39 @@ vtkTimerCallback::~vtkTimerCallback()
 
 void vtkTimerCallback::Execute(vtkObject *vtkNotUsed(caller), unsigned long eventId, void *vtkNotUsed(callData))
 {
-	if (vtkCommand::TimerEvent == eventId)
+	if(vtkCommand::TimerEvent == eventId)
 	{
-		++this->TimerCount;
-		
 		if(this->first_time)
 		{
+			/* on laisse vtk positionner la caméra */
 			this->ptrbotnetgraph->botnet->start();
-			//this->ptrbotnetgraph->botnet->wait();
-			printf("FINITO\n");
-			this->first_time = false;
-		}
-		else
-		{
 			this->ptrbotnetgraph->update_graph();
 			this->ptrbotnetgraph->graphLayoutView->ResetCamera();
 			this->ptrbotnetgraph->graphLayoutView->Render();
+			
+			/* hack = on attrape le renderer */
+			this->ptrbotnetgraph->interactor->FindPokedRenderer(this->ptrbotnetgraph->interactor->GetInteractor()->GetEventPosition()[0], this->ptrbotnetgraph->interactor->GetInteractor()->GetEventPosition()[1]);
+			if (this->ptrbotnetgraph->interactor->GetCurrentRenderer() == NULL)
+    			return;		
+  	
+  			/* on sauvegarde le coefficient du zoom au départ */
+    		vtkCamera *camera = this->ptrbotnetgraph->interactor->GetCurrentRenderer()->GetActiveCamera();
+			if(camera->GetParallelProjection())
+				this->ptrbotnetgraph->interactor->zoom_save = camera->GetParallelScale();
+
+			this->first_time = false;
+		}
+		else
+		{	
+			this->ptrbotnetgraph->update_graph();
+			//this->ptrbotnetgraph->graphLayoutView->ResetCamera();
+			
+			vtkCamera *camera = this->ptrbotnetgraph->interactor->GetCurrentRenderer()->GetActiveCamera();
+			if(camera->GetParallelProjection())
+				camera->SetParallelScale(this->ptrbotnetgraph->interactor->zoom_save);
+			
+			this->ptrbotnetgraph->graphLayoutView->Render();
 		}
 	}
-	
-	cout << this->TimerCount << endl;
 }
 
