e0a291baed6ee8b5b83e8653ba2a000163bc81e4
tathanhdinh <tathanhdinh@gmail.com>
2010-06-14 10:46:29 +0200

    mules

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -1,7 +1,4 @@
 /*
-    <one line to give the library's name and an idea of what it does.>
-    Copyright (C) <year>  <name of author>
-
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
@@ -24,10 +21,78 @@ namespace waledac
 {
 
 
+/*
+ * connection constructor
+ */
 Connection::Connection(bot_t& beginning, bot_t& end)
 {
 	m_beginning = beginning;
 	m_end = end;
+	
+	m_type = CONNECTION_NORMAL;
+}
+
+
+/*
+ * return beginning of connection
+ */
+bot_t Connection::beginning()
+{
+	return m_beginning;
+}
+
+
+/*
+ * return end of connection
+ */
+bot_t Connection::end()
+{
+	return m_end;
+}
+
+
+/*
+ * return type of connection
+ */
+connection_type& Connection::type()
+{
+	return m_type;
+}
+
+
+/*===========================================================================*/
+/*                    implementation of common functions                     */
+/*===========================================================================*/
+
+/*
+ * create and insert connections into a connection list
+ */
+void insert_connections(connections_t& connections, bot_t& beginning, bots_t& ends)
+{
+	connection_t new_connection;
+	for (unsigned int i = 0; i < ends.size(); ++i) {
+		new_connection.reset(new Connection(beginning, ends[i]));
+		connections.push_back(new_connection);
+	}
+		
+}
+
+
+/*
+ * find a connection in a connection list
+ */
+connection_t find_connection(connections_t& connections, bot_t& begin, bot_t& end)
+{
+	connection_t found_connection;
+
+	for (unsigned int i = 0; i < connections.size(); ++i) {
+		if ((connections[i]->beginning()->id() == begin->id()) && 
+			(connections[i]->beginning()->id() == end->id())) {
+			found_connection = connections[i];
+		}
+	}
+	
+	return found_connection;
 }
 
 
